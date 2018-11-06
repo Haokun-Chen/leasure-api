8c207c7a244c2127cc55a5349aab4164e9ab9c24
Haokun Chen <chen.haokun@outlook.com>
2018-11-06 14:13:56 -0500

    modify user model

--- models/user.js
+++ models/user.js
@@ -4,18 +4,18 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-//   firstName: {
-//     type: String,
-//     required: true,
-//     minlength: 5,
-//     maxlength: 50
-//   },
-//   lastName: {
-//     type: String,
-//     required: true,
-//     minlength: 5,
-//     maxlength: 50
-//   },
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50
+  },
   email: {
     type: String,
     required: true,
@@ -26,15 +26,15 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 5,
+    minlength: 8,
     maxlength: 1024,
   },
-//   phone: {
-//     type: Number,
-//     required: true,
-//     minlength: 10,
-//     maxlength: 13,
-//   }
+  phone: {
+    type: Number,
+    // required: true,
+    minlength: 10,
+    maxlength: 10,
+  }
 });
 
 // generate token with user id
@@ -47,9 +47,11 @@ const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
   const schema = {
-    // name: Joi.string().min(5).max(50).required(),
+    firstName: Joi.string().min(1).max(50).required(),
+    lastName: Joi.string().min(1).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required()
+    password: Joi.string().min(8).max(1024).required(),
+    phone: Joi.number().min(1000000000)
   };
 
   return Joi.validate(user, schema);
